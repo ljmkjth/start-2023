ec4397f335310c1ace91d3ed78c09721dcced46a
jie.liu <815104595@qq.com>
2023-07-18 18:03:24 +0800

    the first commit

---
+++ app/src/main/java/com/free/jie/MainActivity.java
@@ -0,0 +1,20 @@
+package com.free.jie;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author : jie.liu
+ * @date : 2023/7/18 5:46 PM
+ * @function
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
